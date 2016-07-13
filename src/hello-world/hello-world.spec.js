009c50e03d37198c9d86d81b78791c1842bf7318
Fernando Pasik <fernando@pasik.com.ar>
2016-07-13 01:33:55 -0300

    refactor: How to mount styles on tests and clean coverage folder

--- src/hello-world/hello-world.spec.js
+++ src/hello-world/hello-world.spec.js
@@ -4,6 +4,17 @@ import HelloWorld from './hello-world.js';
 import HelloWorldStyles from './hello-world.scss';
 import React from 'react';
 
+let styles;
+
+before(() => {
+  styles = mount(<style>{ HelloWorldStyles }</style>,
+    { attachTo: document.head });
+});
+
+after(() => {
+  styles.detach();
+});
+
 describe('HelloWorld', () => {
 
   it('has a default who value', () => {
@@ -17,11 +28,11 @@ describe('HelloWorld', () => {
   });
 
   it('use blue color for text', () => {
-    mount(<style>{ HelloWorldStyles }</style>, { attachTo: document.head });
     const
       wrapper = mount(<HelloWorld />, { attachTo: setReactRoot() }),
       $helloWorld = wrapper.find('.hello-world').get(0);
 
+    getCSSValue($helloWorld, 'color').should.not.be.empty();
     getCSSValue($helloWorld, 'color').should.be.colored('#3a99d8');
   });
 
