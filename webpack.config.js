4ec7c98e0ce1aef0c2854bf5c6d9984e4e0ae374
Fernando Pasik <fernando@pasik.com.ar>
2019-02-23 21:43:39 +0000

    fix: babel loader wrong name

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ const common = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['@babel/loader'],
+        use: ['babel-loader'],
         enforce: 'post',
       },
       {
@@ -77,7 +77,7 @@ const common = {
       },
       {
         test: /\.md$/,
-        use: ['@babel/loader', 'react-markdown-loader'],
+        use: ['babel-loader', 'react-markdown-loader'],
         enforce: 'post',
       },
     ],
