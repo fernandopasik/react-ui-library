90507e7296a727fff1fede6065edf54c873f4a2a
Fernando Pasik <fernando@pasik.com.ar>
2019-02-23 20:44:59 +0000

    fix: jest config option

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   setupFiles: [
     require.resolve('raf/polyfill'),
   ],
-  setupTestFrameworkScriptFile: '<rootDir>/test/setup.js',
+  setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
   moduleNameMapper: { '\\.(scss)$': '<rootDir>/test/setup.sass.js' },
   coveragePathIgnorePatterns: [
     '<rootDir>/src/utils/',
